8c32859cbdbd2916dca3bfc4d42fb4308a4dc8bf
lucaskripper <kripperlm@gmail.com>
2022-12-22 21:18:10 -0300

    se modificaron metodos write, get y se agrego metodo deletbyId

--- clases.js
+++ clases.js
@@ -2,11 +2,9 @@ const fs = require("fs")
 class ProductManager
 {
     static id=0
-    productList
     constructor(path)
     {
         this.path = path
-        this.productList=[]
     }
     addProduct=(title, description, price, thumbnail, code, stock)=>
     {
@@ -19,44 +17,61 @@ class ProductManager
             stock,
             id : this.#getId()
         }
-        if(fs.existsSync(this.path)){
-            this.productList=this.getProducts()
-            this.productList.push(product)
-            try {
-                fs.writeFileSync(this.path, JSON.stringify(this.productList),"utf-8")
-                console.log("File written succesfully")
-            } catch (error) {
-                throw new Error(error);
-            }
-        }else
+        const productList = this.getProducts()
+        productList.push(product)
+        try 
         {
-            try {
-                fs.writeFileSync(this.path, JSON.stringify(product),"utf-8")
-                console.log("File written succesfully")
-            } catch (error) {
-                throw new Error(error);
-            }
+            fs.writeFileSync(this.path, JSON.stringify(productList),"utf-8")
+            console.log("File written succesfully")
+        } catch (error) 
+        {
+            throw new Error(error);
         }
 
     }
     getProducts=()=>
     {
-        try {
-            const products = JSON.parse(fs.readFileSync(this.path, "utf-8"))
-            return products
-        } catch (error) {
-            throw new Error(error);
+        if(fs.existsSync(this.path))
+            {
+            try {
+                const products = JSON.parse(fs.readFileSync(this.path, "utf-8"))
+                return products
+            } catch (error) {
+                throw new Error(error);
+            }}
+        else
+        {
+            return []
         }
     }
     getProductsById=(id)=>
     {
+        const products = this.getProducts();
+        if(products !== [])
+        {
+            try {
+                const product = products.find(prod=>prod.id===id)
+                return product ? product : null
+                
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+        else
+        {
+            return null
+        }
+    }
+    deletById=(id)=>
+    {
+        const products = this.getProducts()
         try {
-            const products = [] 
-            products = JSON.parse(fs.readFileSync(this.path, "utf-8"))
-            return products.find(prod=>prod.id===id)
-            
+            products = products.filter(prod=>prod.id != id)
+            fs.writeFileSync(this.path, JSON.stringify(products),"utf-8")
+            console.log("Updated File");
+
         } catch (error) {
-            throw new Error(error);
+            throw new Error(error)
         }
     }
     #getId = ()=>
