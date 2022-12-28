301e4d47e66bc37d58bfaf0dc26b61519b8c5ec4
lucaskripper <kripperlm@gmail.com>
2022-12-28 17:32:18 -0300

    se arreglo lo que no funcionaba

--- clases.js
+++ clases.js
@@ -34,7 +34,7 @@ class ProductManager
         if(fs.existsSync(this.path))
             {
             try {
-                const products = JSON.parse(fs.readFileSync(this.path, "utf-8"))
+                const products = JSON.parse([fs.readFileSync(this.path, "utf-8")])
                 return products
             } catch (error) {
                 throw new Error(error);
@@ -51,7 +51,7 @@ class ProductManager
         {
             try {
                 const product = products.find(prod=>prod.id===id)
-                return product ? product : null
+                return product ? product : "No se encontro el producto"
                 
             } catch (error) {
                 throw new Error(error);
@@ -59,23 +59,45 @@ class ProductManager
         }
         else
         {
-            return null
+            return "La lista se encuentra vacia"
         }
     }
     deletById=(id)=>
     {
         const products = this.getProducts()
-        const product = products.find(prod=>prod.id != id)
-        if(product){
-            const index = products.indexOf(product)
-            product.splice(index,1);
+        if(products !== []){
+            const newProducts = products.filter(prod=>prod.id !== id)
             try {
-                fs.writeFileSync(this.path, JSON.stringify(products),"utf-8")
-                console.log("Updated File");
+                fs.writeFileSync(this.path, JSON.stringify(newProducts),"utf-8")
+                console.log("Deleted File");
                 
             } catch (error) {
                 throw new Error(error)
             }
+        }
+        else
+        {
+            console.log("No hay productos");
+        }
+    }
+    updateProduct=(id)=>
+    {
+        const products = this.getProducts();
+        if(products!==[])
+        {
+            const product = products.find(prod => prod.id === id)
+            if(product)
+            {
+                
+            }
+            else
+            {
+                console.log("No se encontro el producto")
+            }
+        }
+        else
+        {
+            console.log("No hay productos");
         }
     }
     #getId = ()=>
